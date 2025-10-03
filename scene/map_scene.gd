088665ad0c72ea913aae878f9baba6d2a1fe82f3
Khongkung <khongmanadi@gmail.com>
2025-10-03 23:47:14 +0700

    add gun to ocean

---
+++ scene/map_scene.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+func _on_gun_fire(bullet_obj, muzzle, bullet_speed) -> void:
+	bullet_obj.hit.connect(_on_explode)
+	add_child(bullet_obj)
+	bullet_obj.global_transform = muzzle.global_transform
+	var forward_vector: Vector3 = bullet_obj.global_transform.basis.y
+	bullet_obj.linear_velocity = forward_vector * bullet_speed
+	
+func _on_explode(explosion_sfx, explosion_effect, hit_position):
+	add_child(explosion_sfx)
+	explosion_sfx.play()
+	add_child(explosion_effect)
+	explosion_effect.global_position = hit_position
+	
+	
